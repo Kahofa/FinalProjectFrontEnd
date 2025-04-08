2729b88c691785ede30c7cf44a8fd5a0eed0a1a7
Malik <maliknagumanov@gmail.com>
2025-04-09 00:52:48 +0500

    чутка доделал

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import f1 from "./images/f1.mp4";
 import godofwar from "./images/godofwar.jpg";
 import lastofus from "./images/lastofus.jpg";
 import cyberpunk from "./images/cyberpunk.jpg";
+import { FixedSizeList as List } from "react-window";
+
 
 function App() {
   const [searchQuery, setSearchQuery] = useState("");
@@ -15,8 +17,6 @@ function App() {
   const [recommendations, setRecommendations] = useState(
     JSON.parse(localStorage.getItem("recommendations")) || []
   );
-
-
 
   const sliderGames = [
     { title: "God of War", genre: "Экшен, мифология", img: godofwar },
@@ -30,8 +30,38 @@ function App() {
     { title: "Red Dead Redemption 2", genre: "Вестерн, открытый мир", img: "rdr2.jpg" },
     { title: "Horizon Forbidden West", genre: "Фантастика, приключения", img: "horizonfw.jpg" },
     { title: "Ghost of Tsushima", genre: "Самураи, экшен", img: "ghostoftsushima.jpg" },
-    { title: "Dark Souls III", genre: "Экшен, хардкор", img: "darksouls3.jpg" }
+    { title: "Dark Souls III", genre: "Экшен, хардкор", img: "darksouls3.jpg" },
+    { title: "Assassin's Creed Valhalla", genre: "Приключения, исторический экшен", img: "img: https://via.placeholder.com/150" },
+    { title: "Cyberpunk 2077", genre: "Футуристический экшен-RPG", img: "cimg: https://via.placeholder.com/150" },
+    { title: "GTA V", genre: "Открытый мир, криминал", img: "gtav.jpg" },
+    { title: "Far Cry 6", genre: "Шутер, открытый мир", img: "farcry6.jpg" },
+    { title: "Resident Evil Village", genre: "Хоррор, выживание", img: "residentevil8.jpg" },
+    { title: "Death Stranding", genre: "Постапокалипсис, доставка", img: "deathstranding.jpg" },
+    { title: "Sekiro: Shadows Die Twice", genre: "Самураи, souls-like", img: "sekiro.jpg" },
+    { title: "Call of Duty: Modern Warfare II", genre: "Шутер, война", img: "codmw2.jpg" },
+    { title: "Battlefield 2042", genre: "Шутер, мультиплеер", img: "bf2042.jpg" },
+    { title: "Overwatch 2", genre: "Командный шутер", img: "overwatch2.jpg" },
+    { title: "Valorant", genre: "Тактический шутер", img: "valorant.jpg" },
+    { title: "Apex Legends", genre: "Батл-рояль, экшен", img: "apex.jpg" },
+    { title: "League of Legends", genre: "MOBA", img: "lol.jpg" },
+    { title: "Dota 2", genre: "MOBA", img: "dota2.jpg" },
+    { title: "Minecraft", genre: "Песочница, выживание", img: "minecraft.jpg" },
+    { title: "Terraria", genre: "Пиксельная песочница", img: "terraria.jpg" },
+    { title: "Hollow Knight", genre: "Метроидвания, платформер", img: "hollowknight.jpg" },
+    { title: "Celeste", genre: "Платформер, история", img: "celeste.jpg" },
+    { title: "Stardew Valley", genre: "Ферма, симулятор", img: "stardew.jpg" },
+    { title: "The Sims 4", genre: "Симулятор жизни", img: "sims4.jpg" },
+    { title: "Planet Zoo", genre: "Симулятор зоопарка", img: "planetzoo.jpg" },
+    { title: "FIFA 23", genre: "Футбол, спорт", img: "fifa23.jpg" },
+    { title: "NBA 2K24", genre: "Баскетбол, спорт", img: "nba2k24.jpg" },
+    { title: "Forza Horizon 5", genre: "Гонки, открытый мир", img: "forza5.jpg" },
+    { title: "Gran Turismo 7", genre: "Гонки, симулятор", img: "gt7.jpg" },
+    { title: "Need for Speed Heat", genre: "Гонки, аркада", img: "nfsheat.jpg" },
+    { title: "It Takes Two", genre: "Кооператив, приключения", img: "ittakestwo.jpg" },
+    { title: "Little Nightmares II", genre: "Хоррор, платформер", img: "ln2.jpg" },
+    { title: "Cuphead", genre: "Платформер, ретро-стиль", img: "cuphead.jpg" },
   ];
+  
 
   useEffect(() => {
     setFilteredGames(
@@ -114,15 +144,25 @@ function App() {
               onChange={(e) => setSearchQuery(e.target.value)}
               className="search-input"
             />
-            <div className="game-grid">
-              {filteredGames.map((game, index) => (
-                <div key={index} className="game-card">
-                  <img src={game.img} alt={game.title} />
-                  <h3>{game.title}</h3>
-                  <p>{game.genre}</p>
-                </div>
-              ))}
-            </div>
+           <List
+              height={400}
+              itemCount={filteredGames.length}
+              itemSize={180}
+              width={"100%"}
+            >
+              {({ index, style }) => {
+                console.log("Рендерю элемент:", index);
+                const game = filteredGames[index];
+                return (
+                  <div style={style} className="game-card">
+                    <img src={game.img} alt={game.title} />
+                    <h3>{game.title}</h3>
+                    <p>{game.genre}</p>
+                  </div>
+                );
+              }}
+          </List>
+
           </div>
         </div>
       
